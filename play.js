8036f180da94f3d129b8adb7970612541befb02e
ismail aissami <ismail.aissami@ens.uvsq.fr>
2020-10-08 00:58:52 +0200

    first commit

---
+++ play.js
@@ -0,0 +1,23 @@
+/* a test
+console.log("tous va bien! play js");
+*/
+
+const question = document.getElementById('question');
+const choices = array.from(
+	document.getElementByClassName('choice-text')
+	);
+
+let currentQuestion = {};
+let acceptingAnswer = true;
+let score = 0, questionCounter = 0;
+
+/*
+	For not repeating the same question a second time !! 
+*/
+let availableQuestions = [];
+
+let question = [
+	/*
+		I need  to put some questions to test 
+	*/
+]
